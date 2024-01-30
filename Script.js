993c1f3a1d12cb949b7e38bf0902929897892919
Pawel Wujczyk <pwujczyk@gmail.com>
2024-01-30 22:32:26 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,22 +1,32 @@
-function SaveAttachement(attachment, targetDirectory){
+function SaveAttachement(attachment, targetDirectory) {
   console.log(attachment.getName())
-  var newsweek=DriveApp.getFoldersByName(targetDirectory).next();
+  var newsweek = DriveApp.getFoldersByName(targetDirectory).next();
   newsweek.createFile(attachment);
   console.log(newsweek.getName());
 }
 
+function sendThankYou(thread) {
+  //console.log(thread);
+
+  }
+
 function myFunction() {
 
-var searchQuery=PropertiesService.getScriptProperties().getProperty("SearchQuery");
-var targetDirectory=PropertiesService.getScriptProperties().getProperty("TargetDirectory");
+  var searchQuery = PropertiesService.getScriptProperties().getProperty("SearchQuery");
+  var targetDirectory = PropertiesService.getScriptProperties().getProperty("TargetDirectory");
 
   var threads = GmailApp.search(searchQuery)
-  var msgs = GmailApp.getMessagesForThreads(threads);
-  for (var i = 0; i < msgs.length; i++) {
-    for (var j = 0; j < msgs[i].length; j++) {
-      var attachments = msgs[i][j].getAttachments();
+  for (var t = 0; t < threads.length; t++) {
+    var thread = threads[t]
+    var msgs = thread.getMessages();
+    for (var j = 0; j < msgs.length; j++) {
+      var attachments = msgs[j].getAttachments();
       for (var k = 0; k < attachments.length; k++) {
+        console.log("saving attachement")
         SaveAttachement(attachments[k], targetDirectory)
+        sendThankYou(thread);
+        
+        thread.reply("Dziękuje")
       }
     }
   }
